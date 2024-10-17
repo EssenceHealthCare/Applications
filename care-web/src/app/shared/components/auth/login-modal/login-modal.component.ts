6879a06db0c206f9e5cd15deaa3a8cc34b62730c
Ben Chuunga <benjamin.chuunga@gmail.com>
2024-10-17 11:28:59 -0400

    Updating ng app.

--- care-web/src/app/shared/components/auth/login-modal/login-modal.component.ts
+++ care-web/src/app/shared/components/auth/login-modal/login-modal.component.ts
@@ -1,6 +1,8 @@
 import { Component } from '@angular/core';
 import { NgbActiveModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
 import { SignupModalComponent } from '../signup-modal/signup-modal.component';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { AuthService } from '../../../api/auth-api';
 
 @Component({
   selector: 'care-web-login-modal',
@@ -8,10 +10,36 @@ import { SignupModalComponent } from '../signup-modal/signup-modal.component';
   styleUrls: ['./login-modal.component.scss'],
 })
 export class LoginModalComponent {
+  form: FormGroup = new FormGroup({});
+  submitted = false;
+
   constructor(
     public activeModal: NgbActiveModal,
-    private readonly offCanvasService: NgbOffcanvas
-  ) {}
+    private readonly offCanvasService: NgbOffcanvas,
+    private readonly fb: FormBuilder,
+    private readonly authService: AuthService
+  ) {
+    this.form = this.fb.group({
+      email: ['', Validators.required],
+      password: ['', Validators.required],
+    });
+  }
+
+  get f() {
+    return this.form.controls;
+  }
+
+  onSubmit() {
+    this.submitted = true;
+
+    if (this.form.invalid) {
+      return;
+    }
+
+    this.authService.login(this.form.value).subscribe((user) => {
+      console.log(user);
+    });
+  }
 
   signup() {
     this.activeModal.close();
