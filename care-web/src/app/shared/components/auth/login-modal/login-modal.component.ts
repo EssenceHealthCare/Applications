a92c1bd098ba63ef695d6f27011a72928f70472d
Ben Chuunga <benjamin.chuunga@gmail.com>
2024-10-17 11:14:25 -0400

    Updating ng app.

--- care-web/src/app/shared/components/auth/login-modal/login-modal.component.ts
+++ care-web/src/app/shared/components/auth/login-modal/login-modal.component.ts
@@ -1,10 +1,26 @@
 import { Component } from '@angular/core';
+import { NgbActiveModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { SignupModalComponent } from '../signup-modal/signup-modal.component';
 
 @Component({
   selector: 'care-web-login-modal',
   templateUrl: './login-modal.component.html',
-  styleUrls: ['./login-modal.component.scss']
+  styleUrls: ['./login-modal.component.scss'],
 })
 export class LoginModalComponent {
+  constructor(
+    public activeModal: NgbActiveModal,
+    private readonly offCanvasService: NgbOffcanvas
+  ) {}
 
+  signup() {
+    this.activeModal.close();
+
+    setTimeout(() => {
+      const offCanvasRef = this.offCanvasService.open(SignupModalComponent, {
+        animation: true,
+        position: 'end',
+      });
+    }, 100);
+  }
 }
--- care-web/src/app/shared/components/layout/header/header.component.ts
+++ care-web/src/app/shared/components/layout/header/header.component.ts
@@ -1,10 +1,29 @@
 import { Component } from '@angular/core';
+import { NgbModal, NgbOffcanvas } from '@ng-bootstrap/ng-bootstrap';
+import { LoginModalComponent } from '../../auth/login-modal/login-modal.component';
+import { SignupModalComponent } from '../../auth/signup-modal/signup-modal.component';
 
 @Component({
   selector: 'care-web-header',
   templateUrl: './header.component.html',
-  styleUrls: ['./header.component.scss']
+  styleUrls: ['./header.component.scss'],
 })
 export class HeaderComponent {
+  constructor(
+    private modalService: NgbModal,
+    private readonly offCanvasService: NgbOffcanvas
+  ) {}
 
+  login() {
+    const modalRef = this.modalService.open(LoginModalComponent, {
+      centered: true,
+    });
+  }
+
+  signup() {
+    const offCanvasRef = this.offCanvasService.open(SignupModalComponent, {
+      animation: true,
+      position: 'end',
+    });
+  }
 }
